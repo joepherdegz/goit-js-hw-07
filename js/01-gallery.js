6e321398e045f15bbc6b1a4223317d850fbac5ea
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-02-10 07:07:12 -0800

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="javascript:void(0)">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+        </a>
+    </li> `;
+    gallery.innerHTML += li;
+});
+
+gallery.addEventListener("click", (event) => {
+    if (event.target.tagName === "IMG") {
+        let bigImage = event.target.getAttribute("data-source");
+        let instance = basicLightbox.create(`<img src="${bigImage}" width="800" height="600"/> `);
+        instance.show();
+    }
+}
+)
